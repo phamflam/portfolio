ff9291d8ba7ba4e6a45ec91ea649ee1a281eb2e8
phamflam <lynnphamthu@gmail.com>
2021-10-03 18:52:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          COOL <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+whateva        </a>
       </header>
     </div>
   );
