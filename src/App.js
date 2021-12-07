f294482c3caa854b64ed4200d3be1a41b2acb9bb
phamflam <lynnphamthu@gmail.com>
2021-12-07 20:14:25 +0100

    add basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+// import tft from './images/TFT.jpg';
+// import gude from "./images/gudetama.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          COOL <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-bitte..        </a>
-      </header>
+    <div className="content">
+      <h1>Lynn Pham</h1>
+      <div id="home" className="screen">
+        Welcome to my webportfolio made with React.js
+        <div >
+          {/* <img id="bg" src={tft} alt="tft"/> */}
+        </div>
+      </div>
+      <div id="about" className="screen">
+        I study applied computer science at HTW Berlin.
+      </div>
+      <div id="projects" className="screen">
+        You can find most of my projects on Github.
+      </div>
+      <div id="contact" className="screen">
+        Email <br />
+        Github <br />
+        LinkedIn
+      </div>
+
     </div>
+
   );
 }
 
